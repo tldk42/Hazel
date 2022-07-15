ae3db889597a086c3e0a482782893803993c740e
Jacob <tldk47@hanmail.net>
2022-07-15 20:27:42 +0900

    Setup Basic Application and Entry point

---
+++ Hazel/src/Hazel/Application.cpp
@@ -0,0 +1,17 @@
+#include "Application.h"
+
+namespace Hazel
+{
+	Hazel::Application::Application()
+	{
+	}
+
+	Hazel::Application::~Application()
+	{
+	}
+
+	void Application::Run()
+	{
+		while (true);
+	}
+}
