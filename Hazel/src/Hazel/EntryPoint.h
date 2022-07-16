e194af5c3f9d47e7a210f3bbedfb06b029d7d5be
Jacob <tldk47@hanmail.net>
2022-07-16 09:57:15 +0900

    spdlog Logging

--- Hazel/src/Hazel/EntryPoint.h
+++ Hazel/src/Hazel/EntryPoint.h
@@ -6,6 +6,10 @@ extern Hazel::Application* Hazel::CreateApplication();
 
 void main(int argc, char** argv)
 {
+	Hazel::Log::Init();
+	HZ_CORE_WARN("Initialized Log");
+	HZ_INFO("Hi");
+
 	auto app = Hazel::CreateApplication();
 	app->Run();
 	delete app;
